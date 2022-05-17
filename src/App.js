09a88b91091c57d3ac7380987048abd17e191dc5
Shertor <shertors@gmail.com>
2022-05-17 14:30:48 +0300

    Функциональный элемент и контекст

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
+import React from 'react'
 import './App.css'
 import FileInput from './Components/FileInput/FileInput'
+import Loader from './Components/Loader/Loader'
+import LoadingContext from './context'
 
 function App() {
+	const [loading, setLoading] = React.useState(false)
+
+	function isLoadedContext(loaded) {
+		setLoading(loaded)
+	}
+
 	return (
-		<div className="body_wrapper">
-			<FileInput></FileInput>
-		</div>
+		<LoadingContext.Provider value={{ isLoadedContext }}>
+			<div className="body_wrapper">
+				<FileInput></FileInput>
+				{loading ? <Loader /> : <h3>Ожидание файла</h3>}
+			</div>
+		</LoadingContext.Provider>
 	)
 }
 
--- src/Components/FileInput/FileInput.js
+++ src/Components/FileInput/FileInput.js
@@ -1,89 +1,80 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './FileInput.css'
+import LoadingContext from '../../context'
 
-export default class FileInput extends React.Component {
-	constructor(props) {
-		super(props)
-		this.state = {
-			isLoaded: false,
-		}
-		this.fileInput = React.createRef()
-		this.handleChange = this.handleChange.bind(this)
-		this.dropHandler = this.dropHandler.bind(this)
-		this.dragOverHandler = this.dragOverHandler.bind(this)
-	}
+function FileInput() {
+	const { isLoadedContext } = useContext(LoadingContext)
+	const [isLoaded, setIsLoaded] = React.useState(false)
+	const [file, setFile] = React.useState([])
 
-	handleChange(event) {
-		console.log(this.fileInput.current.files)
-		if (
-			this.fileInput.current.files &&
-			this.fileInput.current.files.length > 0
-		) {
-			this.setState({ isLoaded: true })
+	let fileInput = React.createRef()
+
+	function handleChange(event) {
+		console.log(fileInput.current.files)
+		if (fileInput.current.files && fileInput.current.files.length > 0) {
+			setIsLoaded(true)
+			setFile(fileInput.current.files[0])
+			isLoadedContext(true)
 		} else {
-			this.setState({ isLoaded: false })
+			setIsLoaded(false)
+			setFile([])
+			isLoadedContext(false)
 		}
 	}
 
-	dropHandler(event) {
+	function dropHandler(event) {
 		console.log('File(s) dropped')
 		event.preventDefault()
 
 		if (event.dataTransfer.files && event.dataTransfer.files.length > 0) {
 			console.log(event.dataTransfer.files[0])
-			this.fileInput.current.files = event.dataTransfer.files
-			this.handleChange()
+			fileInput.current.files = event.dataTransfer.files
+			handleChange()
 		}
 	}
 
-	dragOverHandler(event) {
+	function dragOverHandler(event) {
 		console.log('File(s) in drop zone')
 		event.preventDefault()
 	}
 
-	render() {
-		return (
-			<React.Fragment>
-				<div className="file_input">
-					<div
-						id="drop_zone"
-						onDrop={this.dropHandler}
-						onDragOver={this.dragOverHandler}
-					>
-						{this.state.isLoaded ? (
-							<div>
-								<p>{this.fileInput.current.files[0].name}</p>
-								<p></p>
-							</div>
-						) : (
-							<div>
-								<p>Перетащите файл сюда</p>
-								<p>или нажмите кнопку</p>
-							</div>
-						)}
-					</div>
-					<div className="field__wrapper">
-						<input
-							type="file"
-							ref={this.fileInput}
-							id="field__file-2"
-							className="field field__file"
-							onChange={this.handleChange}
-						/>
-
-						<label className="field__file-wrapper" htmlFor="field__file-2">
-							{this.state.isLoaded ? (
-								<div className="field__file-fake">
-									{this.fileInput.current.files[0].name}
-								</div>
-							) : (
-								<div className="field__file-fake">Файл не выбран</div>
-							)}
-							<div className="field__file-button">Выбрать файл</div>
-						</label>
-					</div>
+	return (
+		<React.Fragment>
+			<div className="file_input">
+				<div id="drop_zone" onDrop={dropHandler} onDragOver={dragOverHandler}>
+					{isLoaded ? (
+						<div>
+							<p>{file.name}</p>
+							<p></p>
+						</div>
+					) : (
+						<div>
+							<p>Перетащите файл сюда</p>
+							<p>или нажмите кнопку</p>
+						</div>
+					)}
 				</div>
-			</React.Fragment>
-		)
-	}
+				<div className="field__wrapper">
+					<input
+						type="file"
+						ref={fileInput}
+						id="field__file-2"
+						className="field field__file"
+						onChange={handleChange}
+					/>
+
+					<label className="field__file-wrapper" htmlFor="field__file-2">
+						{isLoaded ? (
+							<div className="field__file-fake">{file.name}</div>
+						) : (
+							<div className="field__file-fake">Файл не выбран</div>
+						)}
+						<div className="field__file-button">Выбрать файл</div>
+					</label>
+				</div>
+			</div>
+		</React.Fragment>
+	)
 }
+
+export default FileInput
