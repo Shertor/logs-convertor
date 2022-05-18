49e69fefec1234fd04f4128435e40c8ae3a1632d
Shertor <shertors@gmail.com>
2022-05-18 14:52:26 +0300

    Уведомление о сохранении

--- src/Components/SaveResult/SaveResult.js
+++ src/Components/SaveResult/SaveResult.js
@@ -1,17 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './SaveResult.css'
 import { save_file } from '../utils'
 
 export default function SaveResult() {
+	const [isOpen, setIsOpen] = useState(false)
+	const [isSaved, setIsSaved] = useState(false)
+
 	function onResultBtn() {
-		save_file().then(console.log)
+		save_file()
+			.then(setIsSaved)
+			.finally(() => {
+				setIsOpen(true)
+			})
 	}
 
 	return (
-		<div className="save-btn_wrapper">
-			<button onClick={onResultBtn} className="save-btn">
-				Сохранить
-			</button>
-		</div>
+		<React.Fragment>
+			<div className="save-btn_wrapper">
+				<button onClick={onResultBtn} className="save-btn">
+					Сохранить
+				</button>
+			</div>
+			{isOpen && (
+				<div className="my-modal">
+					<div className="my-modal-body">
+						{isSaved ? (
+							<h3>Сохранение прошло успешно</h3>
+						) : (
+							<h3>Ошибка сохранения</h3>
+						)}
+						<button onClick={() => setIsOpen(false)} className="save-btn">
+							Закрыть
+						</button>
+					</div>
+				</div>
+			)}
+		</React.Fragment>
 	)
 }
